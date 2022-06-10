b40d4b84f806b5bc12ac56740fc66dc746844794
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-06-11 01:44:49 +0530

    Initial commit

---
+++ test.py
@@ -0,0 +1,29 @@
+from flask import Flask, jsonify, request
+import pickle
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+CORS(app)
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
+
+@app.route('/', methods=['POST'])
+def home():
+    request_data = request.get_json()
+    print(request_data)
+    x_test=[[request_data["age"],request_data["hypertension"],request_data["heartDisease"],request_data["avgGlucose"]]]
+    filename = 'stroke_analysis.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+    pred = loaded_model.predict(x_test)
+    data=str(pred[0])
+    # if (pred[0] == 0):
+    #     data="May have stroke"
+    # else:
+    #     data="Will not have stroke"
+    return jsonify({'data': data})
+
+
+# driver function
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
