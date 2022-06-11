17da8d0a952e3ed37ca9a57749157e2e89d1062d
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-06-11 17:28:32 +0530

    init commit

--- app/test.py
+++ app/test.py
@@ -7,7 +7,7 @@ CORS(app)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 
-@app.route('/',methods=["POST"])
+@app.route('/check',methods=["POST"])
 def home():
     request_data = request.get_json()
     print(request_data)
