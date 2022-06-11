3d1f4892ef4b5d48fc7b1906c07d67466b93e57d
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-06-11 15:23:44 +0530

    init commit

--- test.py
+++ test.py
@@ -1,39 +1,36 @@
 from flask import Flask, request, jsonify, make_response
 import pickle
-from flask_cors import CORS
+from flask_cors import CORS,cross_origin
 
 app = Flask(__name__)
-CORS(app)
+CORS(app,resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
 # app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 
-@app.route('/res', methods=["POST","OPTIONS"])
+@app.route('/res')
 def home():
-    if request.method == "OPTIONS":  # CORS preflight
-        return _build_cors_preflight_response()
-    elif request.method == "POST":
-        request_data = request.get_json()
-        print(request_data)
-        x_test=[[request_data["age"],request_data["hypertension"],request_data["heartDisease"],request_data["avgGlucose"]]]
-        filename = 'stroke_analysis.sav'
-        loaded_model = pickle.load(open(filename, 'rb'))
-        pred = loaded_model.predict(x_test)
-        data=str(pred[0])
-        response=jsonify({'data': data})
-        # response.headers.add("Access-Control-Allow-Origin", "*")
-        return _corsify_actual_response(response)
-
-def _build_cors_preflight_response():
-    response = make_response()
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    response.headers.add('Access-Control-Allow-Headers', "*")
-    response.headers.add('Access-Control-Allow-Methods', "*")
+    request_data = request.get_json()
+    print(request_data)
+    x_test=[[request_data["age"],request_data["hypertension"],request_data["heartDisease"],request_data["avgGlucose"]]]
+    filename = 'stroke_analysis.sav'
+    loaded_model = pickle.load(open(filename, 'rb'))
+    pred = loaded_model.predict(x_test)
+    data=str(pred[0])
+    response=jsonify({'data': data})
+    # response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
-def _corsify_actual_response(response):
-    response.headers.add("Access-Control-Allow-Origin", "*")
-    return response
+# def _build_cors_preflight_response():
+#     response = make_response()
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     response.headers.add('Access-Control-Allow-Headers', "*")
+#     response.headers.add('Access-Control-Allow-Methods', "*")
+#     return response
+#
+# def _corsify_actual_response(response):
+#     response.headers.add("Access-Control-Allow-Origin", "*")
+#     return response
 
 # driver function
 if __name__ == '__main__':
