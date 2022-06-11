844bc74c6d5872be20db8e929d36f1cfbb01c4f9
Debamalya Palai <debamalyapaloi2001@gmail.com>
2022-06-11 15:07:45 +0530

    init commit

--- test.py
+++ test.py
@@ -8,18 +8,21 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 # app.config['JSONIFY_PRETTYPRINT_REGULAR'] = False
 
 
-@app.route('/res', methods=["POST"])
+@app.route('/res', methods=["POST","OPTIONS"])
 def home():
-    request_data = request.get_json()
-    print(request_data)
-    x_test=[[request_data["age"],request_data["hypertension"],request_data["heartDisease"],request_data["avgGlucose"]]]
-    filename = 'stroke_analysis.sav'
-    loaded_model = pickle.load(open(filename, 'rb'))
-    pred = loaded_model.predict(x_test)
-    data=str(pred[0])
-    response=jsonify({'data': data})
-    # response.headers.add("Access-Control-Allow-Origin", "*")
-    return _corsify_actual_response(response)
+    if request.method == "OPTIONS":  # CORS preflight
+        return _build_cors_preflight_response()
+    elif request.method == "POST":
+        request_data = request.get_json()
+        print(request_data)
+        x_test=[[request_data["age"],request_data["hypertension"],request_data["heartDisease"],request_data["avgGlucose"]]]
+        filename = 'stroke_analysis.sav'
+        loaded_model = pickle.load(open(filename, 'rb'))
+        pred = loaded_model.predict(x_test)
+        data=str(pred[0])
+        response=jsonify({'data': data})
+        # response.headers.add("Access-Control-Allow-Origin", "*")
+        return _corsify_actual_response(response)
 
 def _build_cors_preflight_response():
     response = make_response()
